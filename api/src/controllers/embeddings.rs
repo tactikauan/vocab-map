8e463fb5543bd0a643896919e34e05d2a0fc3b6e
Kauan Decarli <kauandecarli@gmail.com>
2025-10-01 22:18:50 -0300

    api: allow specifying amount of words to predict

--- api/src/controllers/embeddings.rs
+++ api/src/controllers/embeddings.rs
@@ -2,11 +2,20 @@ use crate::services::embeddings;
 use crate::utils::response;
 
 use actix_web::{HttpRequest, HttpResponse, Responder, Scope, web};
+use serde::Deserialize;
 
 async fn predict(req: HttpRequest) -> impl Responder {
     let word: String = req.match_info().load().unwrap();
 
-    let words = match embeddings::predict_from_word(&word, 30) {
+    #[derive(Deserialize)]
+    struct Params {
+        count: Option<i64>,
+    }
+
+    let params = web::Query::<Params>::from_query(req.query_string()).unwrap();
+    let count = params.count.unwrap_or_else(|| 30);
+
+    let words = match embeddings::predict_from_word(&word, count) {
         Ok(words) => words,
         Err(e) => return HttpResponse::InternalServerError().json(response::message(e)),
     };
